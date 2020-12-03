044ef95e4636c69c9c63022d0c8b25e2408db698
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-12-03 16:15:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
+  
+  // Add following methods to the class:
+  // veto — returns No, I must disagree
+
 }
-// Add following methods to the class:
-// veto — returns No, I must disagree
+
 // approve — returns You can do that!
 // doCharity — returns I like to help people.
 // releasePressStatement — returns You will see great things from Scuber.
