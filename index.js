041fef50f1aa925f83901e3043c37d8d4241a59a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-12-03 16:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // In index.js, write a constructor function called BoardMember that sets the following properties: name, homeState, and training.
-function BoardMember {
-  constructor(name, homeState, training){
+function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-}
+
   // Add following methods to the class:
   // veto — returns No, I must disagree
   veto(){
